ce245960454a42630aca3c5bf8e325ee90f0aab7
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-11-29 20:31:43 +0200

    Updated views

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingDetailSerializer
 
-# Create your views here.
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
