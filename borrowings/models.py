8a7ecdbaae8c0b74eb8d4c61919eeb6d38dbde7f
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-11-29 20:26:56 +0200

    Create model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,23 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from library import settings
+
+
+def default_expected_date():
+    date_of_borrowed = datetime.date.today()
+    return date_of_borrowed + datetime.timedelta(days=7)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(default=default_expected_date())
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings")
+
+    def __str__(self):
+        return f"{self.book.title} was borrow {self.borrow_date}" \
+               f" expected return {self.expected_return_date}"
