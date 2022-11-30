b3ff10fe71e568f0528f1a4979d8c96f8e636294
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-11-30 16:14:53 +0200

    Added return-borrowing action

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,9 @@
+from django.contrib.admin import actions
 from django.contrib.auth import get_user_model
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -8,6 +11,7 @@ from borrowings.serializers import (
     BorrowingListSerializer,
     BorrowingDetailSerializer,
     BorrowingCreateSerializer,
+    BorrowingReturnSerializer,
 )
 
 
@@ -48,6 +52,9 @@ class BorrowingViewSet(
         if self.action == "create":
             return BorrowingCreateSerializer
 
+        if self.action == "return_book":
+            return BorrowingReturnSerializer
+
         return BorrowingSerializer
 
     def get_permissions(self):
@@ -57,3 +64,23 @@ class BorrowingViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(
+        methods=["PUT"],
+        detail=True,
+        url_path="return-book",
+        permission_classes=[IsAuthenticated],
+    )
+    def return_book(self, request, pk=None):
+        """endpoint for return borrowed book"""
+        borrowing = self.get_object()
+        serializer = self.get_serializer(borrowing, data=self.request.data)
+        book = borrowing.book
+
+        if serializer.is_valid():
+            if not borrowing.actual_return_date:
+                book.inventory += 1
+                book.save()
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
