c2feeeaa6329207ed68568121f479a21ec32d11a
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-11-30 09:16:40 +0200

    Update borrowing list

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -39,9 +39,6 @@ class BorrowingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("id",
-                  "borrow_date",
-                  "expected_return_date",
-                  "actual_return_date",
                   "book",
                   "author",
                   "user",)
